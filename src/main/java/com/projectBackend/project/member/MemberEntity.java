fa464083ac1d3eb968ed69775a12a3e83b8a1ac3
hubjh <haha1057@naver.com>
2024-02-01 12:28:11 +0900

    20240201/황준호/MemberEntity 가입일 속성 추가

--- src/main/java/com/projectBackend/project/member/MemberEntity.java
+++ src/main/java/com/projectBackend/project/member/MemberEntity.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
@@ -27,6 +28,13 @@ public class MemberEntity {
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(name = "registration_date")
+//    private Date registrationDate;
+
+    @Column(name = "registration_date")
+    private LocalDateTime registrationDate;
+
     @Builder
     public MemberEntity(String memberEmail, String memberPassword, String phone, String nickName, Date birth, Authority authority) {
         this.memberEmail = memberEmail;
@@ -35,8 +43,7 @@ public class MemberEntity {
         this.memberNickName = nickName;
         this.birth = birth;
         this.authority = authority;
+//        this.registrationDate = new Date();
+        this.registrationDate = LocalDateTime.now(); // 현재 날짜와 시간을 얻음
     }
-
-
-
 }
